3d1131284ac30f3967eae98f8129eceb393c4772
Crowdbotics <team@crowdbotics.com>
2023-08-28 06:03:40 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "perfect-fit-43113",
+  url: "https://perfect-fit-43113.botics.co",
+  api: "https://perfect-fit-43113.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
